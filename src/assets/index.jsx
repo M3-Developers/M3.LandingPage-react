8969402a0b4c82fda0186eaf5087a63660f2340c
Lucas Machado <lucasmachadoale4@gmail.com>
2025-02-04 18:46:16 -0300

    Feature: created the Members component

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -2,10 +2,12 @@ import Logo from "./images/Logo.png";
 import HeroImage from "./images/image 1.png"
 import Yt_Icon from "./images/Yt_Icon.png";
 import Email_Icon from "./images/mail.svg";
+import ImgTemp from "./images/imgTemp.png"
 
 export {
     Logo,
     HeroImage,
     Yt_Icon,
     Email_Icon,
+    ImgTemp,
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import "../Globals.scss";
 import "../Style/Home.scss";
 import { Yt_Icon, Email_Icon } from "../assets";
+import MembersObject from "../Simutation"; // Arquivo de simulação do BD
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Members from "../components/Member";
+import { useEffect, useState } from "react";
+
+// Simulação dos membros
 
 export default function Home() {
+  const [members, SetMembers] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      SetMembers(MembersObject);
+    }, 500);
+  }, []);
+
   return (
     <div className="Home_Container">
       <Header />
@@ -14,17 +27,28 @@ export default function Home() {
           <h2 className="BlueTitle_Color">Contato</h2>
           <section className="Email_Container">
             <img src={Email_Icon} alt="Icone do Email" />
-            <p className="LightLink_Font">devs.m3group@gmail.com</p>
+            <p className="DarkLink_Font">devs.m3group@gmail.com</p>
           </section>
           <a
             href="https://www.youtube.com/@M3Devs"
             className="Youtube_Container"
           >
             <img src={Yt_Icon} alt="Icone do Youtube" />
-            <p className="LightLink_Font">M3 Devs</p>
+            <p className="DarkLink_Font">M3 Devs</p>
           </a>
         </section>
         
+        <h2 className="Members_Title BlueTitle_Color">Membros do M3</h2>
+        <section className="Members_Container">
+          {members ? (
+            Object.entries(members).map(([Key, Value]) => (
+              <Members key={Key} Member={Value} />
+            ))
+          ) : (
+            <p>Carregando...</p>
+          )
+        }
+        </section>
       </main>
     </div>
   );
